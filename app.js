54ec13abd20dc711396208a1967eb4b8eb9ae0e4
moonligthpr <137637216+moonligthpr@users.noreply.github.com>
2023-10-24 10:11:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const track1 = document.getElementById('track1');
 const track2 = document.getElementById('track2');
 const playTrack1Button = document.getElementById('playTrack1');
 const playTrack2Button = document.getElementById('playTrack2');
-const stopTrack1Button = document.getElementById('stopTrack1');
-const stopTrack2Button = document.getElementById('stopTrack2');
-let audioSource1, audioSource2;
+const crossfadeSlider = document.getElementById('crossfade');
+let audioBuffer1, audioBuffer2;
+let source1, source2;
 
 track1.addEventListener('change', (event) => {
     loadAudio(event.target.files[0], 1);
@@ -20,42 +20,51 @@ function loadAudio(file, trackNumber) {
     reader.onload = async (event) => {
         const audioBuffer = await audioContext.decodeAudioData(event.target.result);
         if (trackNumber === 1) {
-            audioSource1 = audioContext.createBufferSource();
-            audioSource1.buffer = audioBuffer;
+            audioBuffer1 = audioBuffer;
         } else {
-            audioSource2 = audioContext.createBufferSource();
-            audioSource2.buffer = audioBuffer;
+            audioBuffer2 = audioBuffer;
         }
     };
     reader.readAsArrayBuffer(file);
 }
 
 playTrack1Button.addEventListener('click', () => {
-    if (audioSource1) {
-        audioSource1.connect(audioContext.destination);
-        audioSource1.start(0);
+    if (audioBuffer1) {
+        source1 = audioContext.createBufferSource();
+        source1.buffer = audioBuffer1;
+        source1.connect(audioContext.destination);
+        source1.start(0);
     }
 });
 
 playTrack2Button.addEventListener('click', () => {
-    if (audioSource2) {
-        audioSource2.connect(audioContext.destination);
-        audioSource2.start(0);
+    if (audioBuffer2) {
+        source2 = audioContext.createBufferSource();
+        source2.buffer = audioBuffer2;
+        source2.connect(audioContext.destination);
+        source2.start(0);
     }
 });
 
-stopTrack1Button.addEventListener('click', () => {
-    if (audioSource1) {
-        audioSource1.stop();
-        audioSource1.disconnect(audioContext.destination);
+crossfadeSlider.addEventListener('input', () => {
+    const crossfadeValue = crossfadeSlider.value;
+    if (source1 && source2) {
+        const gain1 = audioContext.createGain();
+        const gain2 = audioContext.createGain();
+
+        source1.connect(gain1);
+        source2.connect(gain2);
+
+        gain1.gain.value = 1 - crossfadeValue;
+        gain2.gain.value = crossfadeValue;
+
+        gain1.connect(audioContext.destination);
+        gain2.connect(audioContext.destination);
+
+        source1.stop();
+        source2.stop();
     }
 });
 
-stopTrack2Button.addEventListener('click', () => {
-    if (audioSource2) {
-        audioSource2.stop();
-        audioSource2.disconnect(audioContext.destination);
-    }
-});
 
 
